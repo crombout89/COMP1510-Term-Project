7344066713594bb23a4714ac1f8810cf818b97ba
Richie Zhang <richiezhang@protonmail.com>
2024-11-25 16:38:23 -0800

    Implement generate_item

--- src/config.py
+++ src/config.py
@@ -7,6 +7,10 @@ TREE_SCALE_OPTIONS = [1, 3, 5, 7, 9]
 # Higher number is less likely because they are used as the upper bound of random.randint
 # A value of 5 is a 1/5 probability, a value of 10 is a 1/10 probability, etc
 ANIMAL_PROBABILITY = 5
+SILVERVINE_PROBABILITY = 100
+CATNIP_PROBABILITY = 20
+BERRY_PROBABILITY = 5
 
 # Entity attribute options
 AILMENT_OPTIONS = ["Injured", "Poisoned", "Dehydrated", "Burned", "Sad", "Starving"]
+BERRY_COLOR_OPTIONS = ["Red", "Green", "Blue", "Yellow", "Purple"]
--- src/entity.py
+++ src/entity.py
@@ -2,8 +2,33 @@ import copy
 import random
 import typing
 
-from config import AILMENT_OPTIONS, ANIMAL_PROBABILITY
+from config import (AILMENT_OPTIONS, BERRY_COLOR_OPTIONS, ANIMAL_PROBABILITY, SILVERVINE_PROBABILITY,
+                    CATNIP_PROBABILITY, BERRY_PROBABILITY)
 from character import current_location
+
+
+def generate_item(character: dict, always: bool = False) -> typing.Optional[dict]:
+    def generate_berry_decision():
+        return always or character["InTree"] and random.randint(1, BERRY_PROBABILITY) == 1
+
+    new_item = {
+        "Type": "Item",
+        "Name": "",
+        "Data": None
+    }
+    if random.randint(1, SILVERVINE_PROBABILITY) == 1:
+        new_item["Name"] = "Silvervine"
+        return new_item
+    elif random.randint(1, CATNIP_PROBABILITY) == 1:
+        new_item["Name"] = "Catnip"
+        return new_item
+    elif generate_berry_decision():
+        berry_color = random.choice(BERRY_COLOR_OPTIONS)
+        new_item["Name"] = "Berry"
+        new_item["Data"] = berry_color
+        return new_item
+    else:
+        return None
 
 
 def generate_entity(board: dict, character: dict) -> typing.Optional[dict]:
@@ -28,4 +53,4 @@ def generate_entity(board: dict, character: dict) -> typing.Optional[dict]:
     if random.randint(1, ANIMAL_PROBABILITY) == 1:
         return generate_animal()
     else:
-        return generate_item()
+        return generate_item(character)
