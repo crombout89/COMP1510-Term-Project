bef73ef51a95280957d31713eebc6a66c28c673e
Richie Zhang <richiezhang@protonmail.com>
2024-11-25 19:17:11 -0800

    Implement move

--- src/action.py
+++ src/action.py
@@ -1,3 +1,17 @@
+from board import valid_location
+
+
+def move(character: dict, board: dict, direction: tuple[int, int]) -> bool:
+    coordinate_type = ("Tree" if character["InTree"] else "Ground") + "Coordinates"
+    new_coordinates = (character[coordinate_type][0] + direction[0],
+                       character[coordinate_type][1] + direction[1])
+    if valid_location(board, new_coordinates):
+        character[coordinate_type] = new_coordinates
+        return True
+    else:
+        return False
+
+
 def perform_action(character: dict, board: dict, action: dict) -> bool:
     if action["Type"] == "Move":
         return move(character, board, action["data"])
