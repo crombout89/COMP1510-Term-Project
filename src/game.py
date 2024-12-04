561451dd338221f22a2d769ed5a0186c93fc8e20
Richie Zhang <richiezhang@protonmail.com>
2024-12-04 12:25:50 -0800

    Check that entity is not None
    Was raising 'NoneType' object is not subscriptable when trying to check attributes when no entity is generated.

--- src/game.py
+++ src/game.py
@@ -32,9 +32,9 @@ def game():
         elif action["Type"] == "Move":
             if check_tummy(player):
                 entity = generate_entity(current_board, player)
-                if entity["Type"] == "Animal":
+                if entity and entity["Type"] == "Animal":
                     help_animal(player, entity)
-                elif entity["Type"] == "Item":
+                elif entity and entity["Type"] == "Item":
                     pick_up_item(player, entity)
             else:
                 game_over()
