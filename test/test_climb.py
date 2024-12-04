625886464a44d29aaca59ae1be7c2537b8ab20a0
Caley R <crombout@my.bcit.ca>
2024-12-03 21:31:08 -0800

    Create test_climb.py

--- test/test_climb.py
+++ test/test_climb.py
@@ -13,5 +13,25 @@ class TestClimbFunction(unittest.TestCase):
             (6, 5): None
         }
 
+    def test_climb_success(self):
+        self.character["TreeCoordinates"] = (5, 5)
+        result = climb(self.character, self.board)
+        self.assertTrue(result)
+        self.assertTrue(self.character["InTree"])
+        self.assertEqual(self.character["TreeCoordinates"], (5, 5))
+
+    def test_climb_again(self):
+        self.character["InTree"] = True
+        self.character["TreeCoordinates"] = (5, 5)
+        result = climb(self.character, self.board)
+        self.assertFalse(result)
+        self.assertFalse(self.character["InTree"])
+
+    def test_climb_without_tree(self):
+        self.character["TreeCoordinates"] = (6, 5)
+        result = climb(self.character, self.board)
+        self.assertFalse(result)
+        self.assertFalse(self.character["InTree"])
+
 if __name__ == '__main__':
     unittest.main()
