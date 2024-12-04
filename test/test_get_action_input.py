01f61040c65b75d6bc18510e7e7f38af83ddbe35
Caley R <crombout@my.bcit.ca>
2024-12-03 17:41:32 -0800

    Update test_get_action_input.py

--- test/test_get_action_input.py
+++ test/test_get_action_input.py
@@ -4,6 +4,18 @@ from src.ui import get_action_input
 
 class TestActionInput(unittest.TestCase):
 
+    def setUp(self):
+        self.character = {
+            "Position": (0, 0),
+            "Tummy": 50,
+            "ExtraEnergy": 0,
+            "Inventory": ["Catnip", "Fish"],
+            "Level": 2
+        }
+        self.board = {
+            "Tiles": [["Grass", "Moss"], ["Tree", "Rock"]]
+        }
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     unittest.main()
