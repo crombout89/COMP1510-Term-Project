ae35ba1da5a2df62c9c6d6b9e96468b55c606427
Richie Zhang <richiezhang@protonmail.com>
2024-12-03 23:03:58 -0800

    Change quote marks in test_direction_input_to_action

--- test/test_direction_input_to_action.py
+++ test/test_direction_input_to_action.py
@@ -7,64 +7,64 @@ class TestDirectionInputToAction(TestCase):
     def test_direction_input_to_action_w_upper_case(self):
         actual = direction_input_to_action("W")
         expected = {
-            'Type': 'Move',
-            'Data': (0, -1)
+            "Type": "Move",
+            "Data": (0, -1)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_w_lower_case(self):
         actual = direction_input_to_action("w")
         expected = {
-            'Type': 'Move',
-            'Data': (0, -1)
+            "Type": "Move",
+            "Data": (0, -1)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_a_upper_case(self):
         actual = direction_input_to_action("A")
         expected = {
-            'Type': 'Move',
-            'Data': (-1, 0)
+            "Type": "Move",
+            "Data": (-1, 0)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_a_lower_case(self):
         actual = direction_input_to_action("a")
         expected = {
-            'Type': 'Move',
-            'Data': (-1, 0)
+            "Type": "Move",
+            "Data": (-1, 0)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_s_upper_case(self):
         actual = direction_input_to_action("S")
         expected = {
-            'Type': 'Move',
-            'Data': (0, 1)
+            "Type": "Move",
+            "Data": (0, 1)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_s_lower_case(self):
         actual = direction_input_to_action("s")
         expected = {
-            'Type': 'Move',
-            'Data': (0, 1)
+            "Type": "Move",
+            "Data": (0, 1)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_d_upper_case(self):
         actual = direction_input_to_action("D")
         expected = {
-            'Type': 'Move',
-            'Data': (1, 0)
+            "Type": "Move",
+            "Data": (1, 0)
         }
         self.assertEqual(expected, actual)
 
     def test_direction_input_to_action_d_lower_case(self):
         actual = direction_input_to_action("d")
         expected = {
-            'Type': 'Move',
-            'Data': (1, 0)
+            "Type": "Move",
+            "Data": (1, 0)
         }
         self.assertEqual(expected, actual)
 
