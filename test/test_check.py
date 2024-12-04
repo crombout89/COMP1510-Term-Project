6884e1a324ff996209a5864d122ad08febab79fc
Caley R <crombout@my.bcit.ca>
2024-12-03 21:04:17 -0800

    Create test_check.py

---
+++ test/test_check.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+
+class TestCheckFunction(unittest.TestCase):
+
+    def setUp(self):
+        self.character = {
+            "Tummy": 50,
+            "Level": 2,
+            "Inventory": ["Catnip", "Silvervine"]
+        }
+
+if __name__ == '__main__':
+    unittest.main()
