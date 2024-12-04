7417372a2ff30adcf4c7ed50e786ee4b06d797e6
Caley R <crombout@my.bcit.ca>
2024-12-03 20:25:13 -0800

    Create test_move.py

---
+++ test/test_move.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+from src.action import move
+
+class TestMoveFunction(unittest.TestCase):
+
+    def setUp(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
