5aee5a8ff3586276634fc79318d80c84cb2264b6
Caley R <crombout@my.bcit.ca>
2024-12-03 23:06:03 -0800

    Update test_generate_animal.py

--- test/test_generate_animal.py
+++ test/test_generate_animal.py
@@ -1,10 +1,33 @@
 import unittest
 from src.entity import generate_animal
+from src.config import ANIMAL_OPTIONS
 
 class TestGenerateAnimal(unittest.TestCase):
 
     def setUp(self):
         self.character = {"Level": 3}
+
+    def test_animal_generation(self):
+        animal = generate_animal(self.character)
+
+        # Check the type and name of the animal
+        self.assertEqual(animal["Type"], "Animal")
+        self.assertIn(animal["Name"], ANIMAL_OPTIONS)
+
+        # Check the number of ailments
+        self.assertGreaterEqual(len(animal["Data"]), 1)
+        self.assertLessEqual(len(animal["Data"]), self.character["Level"])
+
+    def test_unique_ailments(self):
+        animal = generate_animal(self.character)
+        self.assertEqual(len(animal["Data"]), len(set(animal["Data"])))
+
+    def test_ailments_count(self):
+        for level in range(1, 4):  # Test levels from 1 to 3
+            self.character["Level"] = level
+            animal = generate_animal(self.character)
+            self.assertGreaterEqual(len(animal["Data"]), 1)
+            self.assertLessEqual(len(animal["Data"]), level)
 
 if __name__ == '__main__':
     unittest.main()
