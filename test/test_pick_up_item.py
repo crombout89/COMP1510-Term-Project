7910d893afa64601a51b7168d8a2e6c1dae4773e
Caley R <crombout@my.bcit.ca>
2024-12-03 19:11:09 -0800

    Update test_pick_up_item.py()

--- test/test_pick_up_item.py
+++ test/test_pick_up_item.py
@@ -5,3 +5,46 @@ from src.ui import pick_up_item
 class TestPickUpItem(unittest.TestCase):
 
     def setUp(self):
+        # Set up a character and entities for testing
+        self.character = {
+            "Inventory": {
+                "Catnip": 0,
+                "SilverVine": 0,
+                "Berries": {
+                    "Red": 0,
+                    "Green": 0,
+                    "Blue": 0,
+                    "Yellow": 0,
+                    "Purple": 0
+                }
+            }
+        }
+        self.valid_item = {
+            "Type": "Item",
+            "Name": "Catnip",
+            "Data": None
+        }
+        self.valid_item_silvervine = {
+            "Type": "Item",
+            "Name": "SilverVine",
+            "Data": None
+        }
+        self.valid_item_berry = {
+            "Type": "Item",
+            "Name": "Berries",
+            "Data": "Red"
+        }
+        self.invalid_item_type = {
+            "Type": "NotItem",
+            "Name": "Stone",
+            "Data": None
+        }
+
+    @patch('builtins.print')
+    def test_pick_up_valid_item(self, mock_print):
+        pick_up_item(self.character, self.valid_item)
+        self.assertEqual(self.character["Inventory"]["Catnip"], 1)
+        mock_print.assert_called_once_with("💼 You picked up a Catnip.")
+
+if __name__ == '__main__':
+    unittest.main()
