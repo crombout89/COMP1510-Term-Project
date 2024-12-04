f3943ecf137f622a9294d7331fae3c11a638a1a6
Caley R <crombout@my.bcit.ca>
2024-12-03 23:04:07 -0800

    Create test_generate_animal.py

---
+++ test/test_generate_animal.py
@@ -0,0 +1,10 @@
+import unittest
+from src.entity import generate_animal
+
+class TestGenerateAnimal(unittest.TestCase):
+
+    def setUp(self):
+        self.character = {"Level": 3}
+
+if __name__ == '__main__':
+    unittest.main()
