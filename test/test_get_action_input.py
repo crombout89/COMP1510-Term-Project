b341b5b64bbfd09446e11779d4cd18b6e097c0f1
Caley R <crombout@my.bcit.ca>
2024-12-04 23:42:36 -0800

    Update test_get_action_input.py

--- test/test_get_action_input.py
+++ test/test_get_action_input.py
@@ -20,32 +20,23 @@ class TestActionInput(unittest.TestCase):
 
     @patch('builtins.input', side_effect=["W"])
     def test_move_action_up(self, mock_input):
-        action = get_action_input(self.character, self.board)
+        action = get_action_input(self.character)
         self.assertEqual(action, {'Data': (0, -1), 'Type': 'Move'})
 
     @patch('builtins.input', side_effect=["A"])
     def test_move_action_left(self, mock_input):
-        action = get_action_input(self.character, self.board)
+        action = get_action_input(self.character)
         self.assertEqual(action, {'Data': (-1, 0), 'Type': 'Move'})
 
     @patch('builtins.input', side_effect=["S"])
     def test_move_action_down(self, mock_input):
-        action = get_action_input(self.character, self.board)
+        action = get_action_input(self.character)
         self.assertEqual(action, {'Data': (0, 1), 'Type': 'Move'})
 
     @patch('builtins.input', side_effect=["D"])
     def test_move_action_right(self, mock_input):
-        action = get_action_input(self.character, self.board)
+        action = get_action_input(self.character)
         self.assertEqual(action, {'Data': (1, 0), 'Type': 'Move'})
-
-    @patch('src.action.eat')  # Mock the eat function
-    def test_eat_action(self, mock_eat):
-        with patch('builtins.input', side_effect=['Eat Catnip']):
-            action = get_action_input(self.character, self.board)
-
-        self.assertEqual(action["Type"], "Eat")
-        self.assertEqual(action["Data"], ["Catnip"])
-        mock_eat.assert_called_once()  # Check if eat was called with the correct item
 
 
 if __name__ == '__main__':
