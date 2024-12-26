a0413398ecb459b8efaefe6d3ce74560cdcd4624
Richie Zhang <richiezhang@protonmail.com>
2024-12-25 19:38:42 -0800

    Create main Qt widget

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,5 @@
 import logging
+import random
 import sys
 import PySide6.QtCore
 
@@ -9,29 +10,52 @@ from src.character import create_character
 from src.sfx import sfx_setup, play_main_game_music
 from src.ui import print_game_backstory
 
+class Main(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        logging.basicConfig(filename="../game.log",
+                            filemode="a",
+                            format="[%(asctime)s %(filename)s %(funcName)s %(msecs)d %(name)s %(levelname)s] %(message)s",
+                            datefmt="%Y-%M-%d %H:%M:%S",
+                            level=logging.DEBUG)
+
+        sfx_setup()
+        ground = generate_ground_board()
+        current_board = ground
+        player = create_character("Mittens")
+        logging.info("PySide6 version: " + PySide6.__version__)
+        logging.info("Qt version used to compile PySide6: " + PySide6.QtCore.__version__)
+        logging.info("Package information: " + str(sys.path[0]) + str(__package__) + str(__name__))
+        logging.info("Game started.")
+        logging.info("Ground board: " + str(ground))
+        logging.info("Character: " + str(player))
+        play_main_game_music()
+
+        self.button = QtWidgets.QPushButton("Show Version")
+        self.text = QtWidgets.QLabel(str(current_board) + str(player))
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.show_version)
+
+    @QtCore.Slot()
+    def show_version(self):
+        self.text.setText(PySide6.__version__ + PySide6.QtCore.__version__ + str(sys.path[0]) + str(__package__) + str(__name__))
 
 def main():
     """
     This is the entry point of the GUI.
     """
-    logging.basicConfig(filename="../game.log",
-                        filemode="a",
-                        format="[%(asctime)s %(filename)s %(funcName)s %(msecs)d %(name)s %(levelname)s] %(message)s",
-                        datefmt="%Y-%M-%d %H:%M:%S",
-                        level=logging.DEBUG)
+    app = QtWidgets.QApplication([])
 
-    sfx_setup()
-    print_game_backstory()
-    ground = generate_ground_board()
-    current_board = ground
-    player = create_character("Mittens")
-    logging.info("PySide6 version: " + PySide6.__version__)
-    logging.info("Qt version used to compile PySide6: " + PySide6.QtCore.__version__)
-    logging.info("Package information: " + str(sys.path[0]) + str(__package__) + str(__name__))
-    logging.info("Game started.")
-    logging.info("Ground board: " + str(ground))
-    logging.info("Character: " + str(player))
-    play_main_game_music()
+    widget = Main()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
